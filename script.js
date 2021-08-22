011f7d5b6f777a8245b67e2d6745a734c5ec7f9b
Wil <wilhelmjanmapogi@gmail.com>
2021-08-22 10:23:18 +0800

    Add scale btn

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const containerDiv = document.getElementById('container');
+// Scale btn
+const scaleInput = document.getElementById('scale');
+scaleInput.value = 10;
 
-const numPixels = 10,
+const numPixels = scaleInput.value,
     totalPixels = numPixels ** 2;
 
 containerDiv.style.cssText = `grid-template-columns: repeat(${numPixels}, 1fr); \
