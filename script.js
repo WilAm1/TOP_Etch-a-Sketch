e4c544f0b13e38844ac1cd64c218a953a67ad61e
Wil <wilhelmjanmapogi@gmail.com>
2021-08-22 11:11:49 +0800

    Clean some code

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-const containerDiv = document.getElementById('container');
-const pixelContainer = document.getElementById('pixel-container');
-let numPixels = 10;
-let totalPixels = numPixels ** 2;
-// Scale btn
+function updateGrid(numPixels) {
+    // Will remove the container div child if it has 
+    containerDiv.removeChild(containerDiv.firstChild);
+    const totalPixels = numPixels ** 2;
+    // Create a new div with grids
+    const pixelContainer = document.createElement('div');
+    pixelContainer.setAttribute('id', 'pixel-container');
+    // Grid Box CSS style. Uses grid-template to adjust rows and columns
+    pixelContainer.style.cssText = `grid-template-columns: repeat(${numPixels}, 1fr); \
+    grid-template-rows: repeat(${numPixels}, 1fr)`;
+    // Adds created div to container div
+    containerDiv.appendChild(pixelContainer);
+    // Fills the container with individual pixels
+    for (i = 0; i < totalPixels; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.classList.add(`pixel-${i}`);
+        pixel.addEventListener('mouseover', e => {
+            e.target.style.cssText = "background-color: black;";
+        });
+        pixelContainer.appendChild(pixel);
+    }
+}
 
+
+// Divs
+const containerDiv = document.getElementById('container');
+
+
+// Scale btn
 const scaleInput = document.getElementById('scale');
+const scaleIndicator = document.querySelector('.scale-indicator');
+
+// Default Scale number
 scaleInput.value = 10;
+scaleIndicator.textContent = scaleInput.value;
 updateGrid(scaleInput.value);
 
 
 scaleInput.addEventListener('change', e => {
-    const scaleIndicator = document.querySelector('.scale-indicator');
     const scaleValue = e.target.value;
     scaleIndicator.textContent = scaleValue;
-    numPixels = scaleValue;
-    totalPixels = numPixels ** 2;
     updateGrid(scaleValue);
-
 });
-
-
-function updateGrid(numPixels) {
-    containerDiv.removeChild(containerDiv.firstChild);
-    // div id = "pixel-container" > < /div>
-    const pixelContainer = document.createElement('div');
-    pixelContainer.setAttribute('id', 'pixel-container');
-    pixelContainer.style.cssText = `grid-template-columns: repeat(${numPixels}, 1fr); \
-    grid-template-rows: repeat(${numPixels}, 1fr)`;
-    containerDiv.appendChild(pixelContainer)
-    for (i = 0; i < totalPixels; i++) {
-        const pixel = document.createElement('div');
-
-        pixel.classList.add('pixel');
-        pixel.classList.add(`pixel-${i}`);
-        pixelContainer.appendChild(pixel);
-        pixel.addEventListener('mouseover', e => {
-            e.target.style.cssText = "background-color: black;";
-        });
-    }
-}
