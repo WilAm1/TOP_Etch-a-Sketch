ec52020002ad6e2ea5a8a00608615bea68b9e8ff
Wil <wilhelmjanmapogi@gmail.com>
2021-08-22 10:04:15 +0800

    Change placement of column and row grid

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const containerDiv = document.getElementById('container');
 const numPixels = 10,
     totalPixels = numPixels ** 2;
 
-containerDiv.style.cssText = `grid-template-rows: repeat(${numPixels}, 1fr)`
-containerDiv.style.cssText = `grid-template-columns: repeat(${numPixels}, 1fr)`
+containerDiv.style.cssText = `grid-template-columns: repeat(${numPixels}, 1fr); \
+grid-template-rows: repeat(${numPixels}, 1fr)`;
 
 for (i = 0; i < totalPixels; i++) {
     const pixel = document.createElement('div');
